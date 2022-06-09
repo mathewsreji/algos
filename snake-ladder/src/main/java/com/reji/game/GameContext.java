a8c29b97332ef77c20de96fbb60cd2e9dfa7921d
Reji Mathews <contactreji@gmail.com>
2022-06-08 21:46:18 -0400

    Update GameContext.java

--- snake-ladder/src/main/java/com/reji/game/GameContext.java
+++ snake-ladder/src/main/java/com/reji/game/GameContext.java
@@ -37,8 +37,8 @@ public class GameContext implements Runnable{
         while(!isGameOver) {
 
             // game logic here
-            // loop through each player and call play() method by rolling the using Dice.roll()
-                    // On every move, evaluate of a pos manipulator (snake or ladder) is registered with the cell.
+            // loop through each player and call play() method after rolling dice using Dice.roll()
+                    // On every move, evaluate if a pos manipulator (snake or ladder) is registered with the cell.
                     // if so, update player position to a new position accordingly
 
             // at any point if a player touches the final cell, update isGameOver=true. this finishes the GameContext thread
