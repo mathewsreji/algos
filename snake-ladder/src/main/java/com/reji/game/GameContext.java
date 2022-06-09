a967681f7ad89232e564d9377d4ed2fb8c8e73cb
Reji Mathews <contactreji@gmail.com>
2022-06-08 21:35:29 -0400

    Update GameContext.java

--- snake-ladder/src/main/java/com/reji/game/GameContext.java
+++ snake-ladder/src/main/java/com/reji/game/GameContext.java
@@ -27,12 +27,21 @@ public class GameContext implements Runnable{
             throw new CannotStartGameException("Board is not assigned to game context. Please assign a board");
         }
 
+        if(players == null || players.size() <= 0) {
+            throw new CannotStartGameException("No players are registered. Please add some players to the game");
+        }
+
+        // the index of the first player
         int playerIndex = 0;
 
         while(!isGameOver) {
 
+            // game logic here
+            // loop through each player and call play() method by rolling the using Dice.roll()
+                    // On every move, evaluate of a pos manipulator (snake or ladder) is registered with the cell.
+                    // if so, update player position to a new position accordingly
 
-
+            // at any point if a player touches the final cell, update isGameOver=true. this finishes the GameContext thread
         }
     }
 }
